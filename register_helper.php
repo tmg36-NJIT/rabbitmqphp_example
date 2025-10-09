58d88ea03e85abf39fbb48f4e9ddcc63650d567b
Your Name <tmg36@njit.edu>
2025-10-09 00:17:21 -0400

    updated require_once lines w/ absolute paths + fixed syntax

--- register_helper.php
+++ register_helper.php
@@ -3,9 +3,10 @@
 ini_set('display_errors',1);
 error_reporting(E_ALL);
 session_start()
-require_once('path.inc')
-require_once('get_host_info.inc')
-require_once('rabbitMQLib.inc')
+require_once('/var/www/html/path.inc');
+require_once('/var/www/html/get_host_info.inc');
+require_once('/var/www/html/rabbitMQLib.inc');
+
 
 
 ?>
