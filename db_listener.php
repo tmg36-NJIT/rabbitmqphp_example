b50de86cd327512927c99a8f408c46fd3c6c27df
mrifat <mr854@njit.edu>
2025-10-09 16:48:59 -0400

    added login to recieve login requests

--- db_listener.php
+++ db_listener.php
@@ -4,11 +4,15 @@ require_once('path.inc');
 require_once('get_host_info.inc');
 require_once('rabbitMQLib.inc');
 
-function requestHandler($request) {
-	echo "Received request:";
- 	 var_dump($request);
-	return ['success' => true, 'message' => 'Received request'];
-}
+
+function doLoginDB($username, $password) {
+         $mysqli - new mysqli("localhost", "authuser", "StrongPassword123!", "testdb");
+        if($mysqli->connect_errno) {return['success' => false, 'message' => 'the database connection failed']; }
+        $stmt = $mysqli->prepare("SELECT * FROM users WQHERE username=? AND password=?");
+        if(!$stmt) {
+        $ mysqli->close();
+        return ['success' => flase, 'message' => 'query prep failed']; }
+
 
 echo "Database Listener starting\n";
 $server = new rabbitMQServer("testRabbitMQ.ini", "testServer");
@@ -16,4 +20,3 @@ $server->process_requests('requestHandler');
 echo "Database Listener started\n";
 
 ?>
-
