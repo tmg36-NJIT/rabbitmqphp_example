e4dad17de539aeb37d34287bd06c205cbaf020b9
Your Name <tmg36@njit.edu>
2025-10-08 21:55:14 -0400

    reformatted session() placement

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 // This is going to be the official file where Users can register.
 <?php
-session_start();
 error_reporting(E_ALL & ~E_DEPRECATED);
 ini_set('display_errors', 1);
+session_start();
 require_once('/var/www/html/path.inc');
 require_once('/var/www/html/get_host_info.inc');
 require_once('/var/www/html/rabbitMQLib.inc');
