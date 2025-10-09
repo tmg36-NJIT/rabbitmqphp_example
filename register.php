0cdf08e570930ce07db566b839c833b257a527a7
Your Name <tmg36@njit.edu>
2025-10-08 21:48:35 -0400

    added any required dependancies to other files like rabbitMQLib.inc, etc.

--- register.php
+++ register.php
@@ -1,3 +1,10 @@
 // This is going to be the official file where Users can register.
+<?php
+session_start();
 
+require_once('/var/www/html/path.inc');
+require_once('/var/www/html/get_host_info.inc');
+require_once('/var/www/html/rabbitMQLib.inc');
+
+?>
 
