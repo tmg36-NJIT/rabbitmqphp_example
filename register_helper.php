d0afc7d09e5f095431ab024f7385eff749e37ac4
Your Name <tmg36@njit.edu>
2025-10-09 01:34:04 -0400

    request line added to send to rabbitMQ + success/error msg for backend response w/ error logs

--- register_helper.php
+++ register_helper.php
@@ -29,4 +29,16 @@ $req = [
 'password'=>$pass
 ];
 
+try {
+$res = $mq->send_request($req)
+if (!empty($res['success'])) {
+echo htmlspecialchars($res['message']);
+} else {
+echo htmlspecialchars($res['message'] ? 'Unfortunately, Could not register.')
+}
+} catch (Exception $e) {
+echo "Sorry! Looks like we had an eror wile sending request: ".$e->getMessage();
+exit;
+}
 ?>
+
