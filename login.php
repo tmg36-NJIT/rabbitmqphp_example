053fd54e2e2f01ad922dbcc95a58b7c65fb53615
Your Name <tmg36@njit.edu>
2025-10-09 02:02:40 -0400

    Used base setup from register.php to get started (error reporting + session start + dependancies, etc.)

--- login.php
+++ login.php
@@ -1,1 +1,12 @@
 // ok alr so this gonna be the file for the login page
+ <?php
+error_reporting(E_ALL);
+ini_set('display_errors',1);
+session_start();
+
+require_once('/var/www/html/path.inc');
+require_once('/var/www/html/get_host_info.inc');
+require_once('/var/www/html/rabbitMQLib.inc');
+
+$errs = [];
+$okMsg = '';
