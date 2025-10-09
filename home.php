6ee5a244d5c55cb83a7b4d5f6ab2a4e2f7561d3e
Your Name <tmg36@njit.edu>
2025-10-09 14:03:50 -0400

    display some user info for successful logins

--- home.php
+++ home.php
@@ -9,5 +9,13 @@ exit;
 $user = $_SESSION['username'];
 ?>
 
-
+<!DOCTYPE html>
+<html>
+<head><title>Homepage</title></head>
+<body>
+<h2>Welcome, <?php echo htmlspecialchars($user); ?>!</h2>
+<p>Nice. You are logged in, successfully.</p>
+<a href=" ">Logout</a>
+</body>
+</html>
 
