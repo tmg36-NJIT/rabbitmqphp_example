6a8bc9bb445dde60f4d6984372603f53ad716c09
Your Name <tmg36@njit.edu>
2025-10-08 21:52:47 -0400

    added some basic error reporting at the top

--- register.php
+++ register.php
@@ -1,10 +1,12 @@
 // This is going to be the official file where Users can register.
 <?php
 session_start();
-
+error_reporting(E_ALL & ~E_DEPRECATED);
+ini_set('display_errors', 1);
 require_once('/var/www/html/path.inc');
 require_once('/var/www/html/get_host_info.inc');
 require_once('/var/www/html/rabbitMQLib.inc');
+
 
 ?>
 
