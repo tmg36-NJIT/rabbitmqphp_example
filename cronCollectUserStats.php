88e481024974335ab9c7799105c327822690ed44
mrifat <mr854@njit.edu>
2025-10-10 01:52:21 -0400

    added condition for logging connecton errors

--- cronCollectUserStats.php
+++ cronCollectUserStats.php
@@ -6,6 +6,11 @@ date_default_timezone_set('UTC');
 $time=date('Y-m-d H:i:s');
 $logFile='/var/log/user_stats.log';
 
-$conn=new mysqli("localhost","authuser","StrongPassword123!","testdb")
+$conn=new mysqli("localhost","authuser","StrongPassword123!","testdb");
+if($conn->connect_errno){
+file_put_contents($logFile, "[$time] Connection error: ".$conn->connect_error."\n", FILE_APPEND);
+exit;
+
+}
 
 ?>
