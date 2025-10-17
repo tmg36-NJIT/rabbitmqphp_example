d7abf1ca12f3e565d6e946cef7b28827265bc105
Your Name <tmg36@njit.edu>
2025-10-17 02:01:17 -0400

    Some quick setup, initialized PHP strict mode w/ error reporting.

--- rabbit_api.php
+++ rabbit_api.php
@@ -1,1 +1,7 @@
 // this file will handle the logic for deliverables + API calls to be handled through the message broker (RabbitMQ).
+<?php
+declare(strict_types=1);
+error_reporting(E_ALLL);
+ini_set("display_errors, '1');
+
+
