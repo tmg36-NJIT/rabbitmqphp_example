8f93f6bba3cd0539f9a6216dd3ac2d141f707fab
Your Name <tmg36@njit.edu>
2025-10-17 02:14:30 -0400

    Added json response header + fallback logic for user/guest

--- rabbit_api.php
+++ rabbit_api.php
@@ -3,5 +3,6 @@
 declare(strict_types=1);
 error_reporting(E_ALLL);
 ini_set("display_errors, '1');
-
-
+header("Content type: application/json')
+session_start();
+$user = isset($_SESSION['username']) && $_SESSION['username'] !== '' ? $_SESSION['username'] : 'Guest';
