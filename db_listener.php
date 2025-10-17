f2195c54106f4449093fc2b00922053791dfb05f
mrifat <mr854@njit.edu>
2025-10-16 23:57:20 -0400

    new function for reviews + error handling

--- db_listener.php
+++ db_listener.php
@@ -80,6 +80,14 @@ function getGameRecommendations($query = null) {
 	return ['success' => true, 'results' => $data['results'] ?? []];
 }
 
+
+function submitReview($username. $game_name, rating, $review){ 
+	$mysqli- new mysqli("localhost", "authusr", "Strongpassword123!", "testdb;");
+	if($mysqli->connect_errno){return['success' =>false 'message'=> 'database connection failed']}
+
+
+
+
 function requestProcessor($request) {
 	echo "Received request:";
 	 var_dump($request);
