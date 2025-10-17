d2c5ff3d705f6788569d6068a9c6d98ad8cb92c9
Your Name <tmg36@njit.edu>
2025-10-17 02:24:17 -0400

    Added local paths for lib + ini file

--- rabbit_api.php
+++ rabbit_api.php
@@ -6,3 +6,10 @@ ini_set("display_errors, '1');
 header("Content type: application/json')
 session_start();
 $user = isset($_SESSION['username']) && $_SESSION['username'] !== '' ? $_SESSION['username'] : 'Guest';
+
+//some local paths 
+
+$lib = __DIR__ . '/rabbitMQLib.inc';
+$ini = __DIR__ . '/testRabbitMQ.ini';
+$setion = 'testServer';
+
