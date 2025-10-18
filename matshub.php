7503d012f1025dd05526c0ee6911bf4cf4b3b9da
Your Name <tmg36@njit.edu>
2025-10-18 03:21:16 -0400

    initialized session logic + login redirect for unverified users

--- matshub.php
+++ matshub.php
@@ -1,1 +1,21 @@
 // this is going to be the official landing page for the video game recommendation platform (MATS: GameHub)
+<?php
+session_start();
+if(!isset($_SESSION['username'])){
+  header("Location: login.php");
+  exit;
+}
+$username=$_SESSION['username'];
+$email=$_SESSION['email'] ?? '';
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>MATS: GameHub</title>
+</head>
+<body>
+</body>
+</html>
+
+
+?>
