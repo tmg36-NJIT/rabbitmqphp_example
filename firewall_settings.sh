6f61fcb32af282bae18f1fffb727c63b25c7ff55
Ahmed Chowdhury <arc73@njit.edu>
2025-10-19 03:08:34 +0000

    Updates to internally accessible ports

--- firewall_settings.sh
+++ firewall_settings.sh
@@ -18,9 +18,9 @@ sudo ufw allow 443/tcp comment "Allow HTTPS"
 
 #Local System Service Ports
 sudo ufw allow in on lo to any port 53 comment "Allow local DNS"
-sudo ufw allow in on lo to any port 5353/udp comment "Allow local mDNS/Avahi"
 
 #Internal Network Access Ports
+sudo ufw allow from 192.168.192.0/24 to any port 9993/udp comment "Allow ZeroTier Internally"
 sudo ufw allow from 192.168.192.0/24 to any port 22/tcp comment "Allow SSH Internally"
 sudo ufw allow from 192.168.192.0/24 to any port 5672 comment "Allow RabbitMQ Internally"
 sudo ufw allow from 192.168.192.0/24 to any port 15672 comment "Allow RabbitMQ MGMT Internally"
@@ -28,8 +28,10 @@ sudo ufw allow from 192.168.192.0/24 to any port 25672/tcp comment "Allow Rabbit
 sudo ufw allow from 192.168.192.0/24 to any port 4369/tcp comment "Allow Erlang Node Discovery Internally"
 sudo ufw allow from 192.168.192.0/24 to any port 19999/tcp comment "Allow Netdata Dashboard Internally"
 sudo ufw allow from 192.168.192.0/24 to any port 8125/tcp comment "Allow Netdata/statsd Internally"
+sudo ufw allow from 192.168.192.0/24 to any port 8125/udp comment "Allow Netdata/statsd Internally"
 sudo ufw allow from 192.168.192.0/24 to any port 3306/tcp comment "Allow MySQL Server Internally"
 sudo ufw allow from 192.168.192.0/24 to any port 33060/tcp comment "Allow MySQL Plugin Internally"
+sudo ufw allow from 192.168.192.0/24 to any port 5353/udp comment "Allow mDNS/Avahi Internally"
 
 #Deny Unused or Unnecessary Ports
 sudo ufw deny 23/tcp comment "Deny Telnet"
