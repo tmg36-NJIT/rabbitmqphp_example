72cb3689a312d397f6bc98fa88674fc78c3bcd27
mrifat <mr854@njit.edu>
2025-10-19 21:37:48 -0400

    updating cron atuomation for sending emails. added checking watchlist and user emails

--- cronCollectUserStats.php
+++ cronCollectUserStats.php
@@ -4,28 +4,35 @@ error_reporting(E_ALL);
 date_default_timezone_set('UTC');
 
 $time=date('Y-m-d H:i:s');
-$logFile='/var/log/user_stats.log';
+$logFile = '/var/www/html/game_watchlist_cron.log';
 
 $conn=new mysqli("localhost","authuser","StrongPassword123!","testdb");
-if($conn->connect_errno){
-file_put_contents($logFile, "[$time] Connection error: ".$conn->connect_error."\n", FILE_APPEND);
-exit;
+if($conn->connect_errno){file_put_contents($logFile, "[$time] Connection error: ".$conn->connect_error."\n", FILE_APPEND);
+exit;}
 
-}
+//api url+key
+$rawgKey= 'e92e94964e714d64aa425f8c11d0996e';
+$rawgUrl= 'https://api.rawg.io/api/games?key=' . $rawgKey;
+$cheapsharkUrl= 'https://www.cheapshark.com/api/1.0/games?title=';
 
-$query="SELECT COUNT(*) AS total FROM users";
-$data=$conn->query($query);
 
-if(!$data){
-file_put_contents($logFile,"[$time] Query failed: ".$conn->error."\n",FILE_APPEND);
-$conn->close();
-exit;
-}
 
-$count=$data->fetch_assoc(){'total']??0;
-$entry="[$time] Users in table: $count\n";
-file_put_contents($logFile,$entry,FILE_APPEND);
-echo $entry;
-$conn->close();
+$query= "SELECT id, username, game_name, last_known_price, last_release_date, last_updated FROM watchlist";
+$result= $conn->query($query);
+
+if (!$result){ file_put_contents($logFile, "[$time] Query failed: " . $conn->error . "\n", FILE_APPEND);
+	$conn->close();
+	exit;}
+
+
+
+$userChanges= [];
+ while ($row= $result->fetch_assoc(){
+	$gameName= urlencode($row['game_name']);
+	 $changes= [];
+	$newReleaseDetected= false;
+	$patchUpdateDetected= false;
+	$genreString= "";
+
 
 ?>
