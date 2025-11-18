6242e8ff73d88559b84336230f059860fb0ad106
mrifat <mr854@njit.edu>
2025-11-18 00:18:46 -0500

    updating personalized function to use new function

--- dmz_listener.php
+++ dmz_listener.php
@@ -45,15 +45,8 @@ function getGameRecommendations($query = null) {
 
 //getPersonalizedRecommendation is the acutal deliverable for recommendations
 function getPersonalizedRecommendations($username){
-	$mysqli= new mysqli("localhost", "authuser", "StrongPassword123!", "testdb");
-	if($mysqli->connect_errno) return['success'=> false,'message'=> 'DB connection failed'];
-
-	$stmt= $mysqli->prepare("SELECT game_name, rating FROM reviews WHERE username =?");
-	 if(!$stmt){$mysqli->close();
-	return['success'=> false, 'message' =>'Query prep had failed'];}
-	$stmt->bind_param("s", $username);
-	$stmt->execute();
-	$result = $stmt->get_result();
+	$dbReviews= fetchUsrReviewsThruRMQ($username);
+	 if(!$dbReviews['success']) {return ['success' => false, 'message' => 'db error'];}
 
 	$liked= [];
 	$liked_soft=[];
